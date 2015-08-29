32d8da98ceb12d87508287e105aafef829f3efd1
jaimemorais <jaimemorais@gmail.com>
2015-08-29 16:11:40 -0300

    Adding payment type funcionality

---
+++ ExpenseTrackerWeb/Controllers/PaymentTypeController.cs
@@ -0,0 +1,120 @@
+﻿using ExpenseTrackerWeb.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpenseTrackerWeb.Controllers
+{
+    public class PaymentTypeController : BaseController
+    {
+        // GET: PaymentType
+        public async Task<ActionResult> Index()
+        {
+            List<PaymentType> paymentTypes = await base.GetPaymentTypesAsync();
+
+            return View(paymentTypes);
+        }
+
+
+        // GET: PaymentType/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PaymentType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PaymentType/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(PaymentType paymentType)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string url = base.GetApiServiceURL("PaymentTypeApi");
+
+                    var httpClient = new HttpClient();
+                    httpClient.DefaultRequestHeaders.Add("User-Agent", "Other");
+                    var response = await httpClient.PostAsJsonAsync(url, paymentType);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        ShowMessage("Payment Type '" + paymentType.Name + "' added.", EnumMessageType.INFO);
+                    }
+                    else
+                    {
+                        ShowMessage("Error contacting server.", EnumMessageType.ERROR);
+                    }
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                ShowMessage("Error creating new Payment Type.", EnumMessageType.ERROR);
+                return View();
+            }
+        }
+
+        // GET: PaymentType/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentType/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PaymentType/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentType/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ExpenseTrackerWeb/Controllers/RestApi/PaymentTypeApiController.cs
@@ -0,0 +1,77 @@
+﻿using ExpenseTrackerWeb.Helpers;
+using ExpenseTrackerWeb.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ExpenseTrackerWeb.Controllers.RestApi
+{
+
+    public class PaymentTypeApiController : ApiController
+    {
+        // GET api/PaymentTypeApi
+        public async Task<IEnumerable<string>> GetAsync()
+        {
+            MongoHelper<PaymentType> paymentTypeHelper = new MongoHelper<PaymentType>();
+                
+            IList<string> returnList = new List<string>();
+            var jsonWriterSettings = new JsonWriterSettings { OutputMode = JsonOutputMode.Strict };
+            await paymentTypeHelper.Collection.Find(p => p.Name != null) // TODO filter by userId
+                .ForEachAsync(paymentTypeDocument => 
+                {
+                    string docJson = paymentTypeDocument.ToJson(jsonWriterSettings);
+                    returnList.Add(docJson);
+                }
+            );
+
+            return returnList.ToArray();
+        }
+
+        // GET api/PaymentTypeApi/5
+        public string Get(int id)
+        {
+            // TODO get one
+            return "value";
+        }
+
+        // POST api/PaymentTypeApi
+        public async Task PostAsync(PaymentType paymentTypePosted)
+        {
+            MongoHelper<PaymentType> paymentTypeHelper = new MongoHelper<PaymentType>();
+
+            try
+            {
+                await paymentTypeHelper.Collection.InsertOneAsync(paymentTypePosted);
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("PaymentTypeApi PostAsync error : " + e.Message);
+                throw;
+            }
+
+        }
+
+        // PUT api/PaymentTypeApi/5
+        public void Put(int id, [FromBody]string value)
+        {            
+            // TODO update
+        }
+
+        // DELETE api/PaymentTypeApi/5
+        public void Delete(int id)
+        {
+            // TODO delete
+        }
+    }
+}
