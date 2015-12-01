673e5abad5f885d7c162ae0309e38784df9d81ac
Jaime Batista Morais <jaime_morais@hotmail.com>
2015-12-01 16:08:51 -0200

    Testing github tools

--- ExpenseTrackerWeb/Controllers/BaseController.cs
+++ ExpenseTrackerWeb/Controllers/BaseController.cs
@@ -9,7 +9,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ExpenseTrackerWeb.Controllers
