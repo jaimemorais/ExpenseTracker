a6c4cd32b31ca3c74abd14bdf781c06364e70d53
jaimemorais <jaimemorais@gmail.com>
2017-02-09 15:50:01 -0200

    change app icon and label

--- ExpenseTrackerMvp/ExpenseTrackerMvp.Droid/MainActivity.cs
+++ ExpenseTrackerMvp/ExpenseTrackerMvp.Droid/MainActivity.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace ExpenseTrackerMvp.Droid
 {
-    [Activity(Label = "ExpenseTrackerMvp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Expense Tracker", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
