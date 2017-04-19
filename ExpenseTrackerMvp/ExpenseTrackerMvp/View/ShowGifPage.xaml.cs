39249412d3549e9354135393e87eb95a0fcd4eb3
jaimemorais <jaimemorais@gmail.com>
2017-04-19 09:09:43 -0300

    Add test gif page

---
+++ ExpenseTrackerMvp/ExpenseTrackerMvp/View/ShowGifPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using Xamarin.Forms;
+
+namespace ExpenseTrackerMvp.View
+{
+    public partial class ShowGifPage : ContentView
+    {
+        public ShowGifPage()
+        {
+            InitializeComponent();
+
+            /* TODO
+             * https://components.xamarin.com/view/GifImageViewAndroid 
+             * https://github.com/jamesmontemagno/PuppyKittyOverflow/blob/master/PuppyKittyOverflow.Portable/OverflowHelper.cs 
+             * https://github.com/jamesmontemagno/Xamarin.Forms-Awesome-Controls/tree/master/GifImageView
+             * 
+            Content = new GifImageViewControl
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Source = ImageSource.FromResource("GifViewer.dogturtle.gif")
+                //Source = ImageSource.FromUri(new Uri("http://dogoverflow.com/dRX5G8qK"))
+            };
+
+            */
+        }
+    }
+}
