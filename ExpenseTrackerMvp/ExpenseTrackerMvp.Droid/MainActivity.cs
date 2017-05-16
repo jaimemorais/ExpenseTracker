071c2f0058c1f02cb9f03e25fd30c0a8c8b3d494
jaimemorais <jaimemorais@gmail.com>
2017-05-16 17:27:43 -0300

    Add splash screen

--- ExpenseTrackerMvp/ExpenseTrackerMvp.Droid/MainActivity.cs
+++ ExpenseTrackerMvp/ExpenseTrackerMvp.Droid/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace ExpenseTrackerMvp.Droid
 {
-    [Activity(Label = "Expense Tracker", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Expense Tracker", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
