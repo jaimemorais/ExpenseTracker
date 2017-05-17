aa0fe007da1d93f2f1b0bfdcf89a0deb495690d6
jaimemorais <jaimemorais@gmail.com>
2017-05-17 19:15:56 -0300

    Add formatting

--- ExpenseTrackerWeb/Controllers/BalanceController.cs
+++ ExpenseTrackerWeb/Controllers/BalanceController.cs
@@ -3,6 +3,9 @@ using ExpenseTrackerWeb.Filters;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -28,5 +31,59 @@ namespace ExpenseTrackerWeb.Controllers
             }
 
         }
+
+
+        public async Task<ActionResult> BalanceTxt()
+        {
+            try
+            {
+
+                List<Expense> expenses = await base.GetItemListAsync<Expense>("Expenses");
+
+
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine("<table>");
+                
+
+                foreach (Expense exp in expenses)
+                {
+                    sb.AppendLine("<tr>");
+
+                    string month = System.Globalization.DateTimeFormatInfo.CurrentInfo.GetMonthName(exp.Date.Month);
+                    month = month.Substring(0, 1).ToUpper() + month.Substring(1);
+
+                    sb.AppendLine(GetTD(month) +
+                                  GetTD(exp.Date.Day.ToString()) +
+                                  GetTD(exp.Value.ToString()) +
+                                  GetTD(" ???? ") +
+                                  GetTD(exp.Category) +
+                                  GetTD(exp.Description) +
+                                  GetTD((exp.PaymentType == "Cartao Credito" ? "Itau CC" + GetTD("Cartao de Credito") + GetTD(" ???? ") : exp.PaymentType))
+                                 );
+
+                    
+
+                    sb.AppendLine("</tr>");
+                }
+                
+                return Content(sb.ToString(), "text/html");
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("BalanceController Index Error : " + e.Message);
+                ShowMessage("Error getting expense list.", EnumMessageType.ERROR);
+                return View();
+            }
+
+        }
+
+        private string GetTD(string str)
+        {
+            return "<td>" + str + "</td>";
+        }
+
     }
 }
