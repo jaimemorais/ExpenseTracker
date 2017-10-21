eb823674a335636cc1998a9e3fef62bb00e70cb4
jaimemorais <jaimemorais@gmail.com>
2017-10-21 14:49:53 -0200

    Add final app

---
+++ ExpenseTrackerApp/ExpenseTrackerApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using ExpenseTrackerApp.Views;
+using Prism.DryIoc;
+using Xamarin.Forms;
+
+namespace ExpenseTrackerApp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            // TODO register
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
