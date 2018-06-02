017a4f92eee7e04549f6bc6f3c8f2bcbd3d17bf8
jaimemorais <jaimemorais@gmail.com>
2018-06-02 20:00:09 -0300

    Create custom decimal entry

---
+++ ExpenseTrackerApp/ExpenseTrackerApp.Droid/CustomRenderers/DecimalEntryRenderer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpenseTrackerApp.CustomRenderers;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics.Drawables;
+using Android.Text;
+using Android.Views;
+using System;
+using System.ComponentModel;
+using ExpenseTrackerApp.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+
+[assembly: ExportRenderer(typeof(DecimalEntry), typeof(DecimalEntryDroidRenderer))]
+namespace ExpenseTrackerApp.Droid.CustomRenderers
+{
+    public class DecimalEntryDroidRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+
+            if (e.OldElement != null)
+            {
+                Control.AfterTextChanged -= Control_AfterTextChanged;
+            }
+
+            if (e.NewElement != null)
+            {
+                Control.AfterTextChanged += Control_AfterTextChanged;
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(nameof(DecimalEntry.Value)))
+            {
+                var element = ((DecimalEntry)Element);
+
+                // Format number, and place the formatted text in newText
+                var formattedValue = DecimalEntry.ConvertNumber(element.Value);
+
+                if (element.Value == 0) formattedValue = "";
+
+                var newText = formattedValue;
+
+                // Set the Text property of our control to newText
+                Control.Text = newText;
+            }
+            else
+            {
+                base.OnElementPropertyChanged(sender, e);
+            }
+
+        }
+
+
+
+        void Control_AfterTextChanged(object sender, AfterTextChangedEventArgs e)
+        {
+            var element = ((DecimalEntry)Element);
+            
+            // 1. Stop listening for changes on our control Text property
+            if (!element.ShouldReactToTextChanges) return;
+            element.ShouldReactToTextChanges = false;
+
+            // 2. Get the current cursor position
+            var cursorPosition = Control.SelectionStart;
+
+            // 3. Take the control’s text, lets name it oldText
+            var oldText = Control.Text;
+
+            // 4. Parse oldText into a number, lets name it number
+            int number = DecimalEntry.DumbParse(oldText);
+            element.Value = number;
+
+            // 5. Format number, and place the formatted text in newText
+            var valorFormatado = DecimalEntry.ConvertNumber(number);
+
+            if (number == 0) valorFormatado = "";
+
+            var newText = valorFormatado;
+
+            // 6. Set the Text property of our control to newText
+            Control.Text = newText;
+
+            // 7. Calculate the new cursor position
+            var change = oldText.Length - newText.Length;
+
+            // 8. Set the new cursor position
+            int newPos = cursorPosition - change;
+            Control.SetSelection(newPos < 0 ? 0 : newPos);
+
+            // 9. Start listening for changes on our control’s Text property
+            element.ShouldReactToTextChanges = true;
+        }
+
+
+    }
+}
---
+++ ExpenseTrackerApp/ExpenseTrackerApp/CustomRenderers/DecimalEntry.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ExpenseTrackerApp.CustomRenderers
+{
+    public class DecimalEntry : Entry
+    {
+        public static readonly BindableProperty ValueProperty =
+            BindableProperty.Create(nameof(Value), typeof(int), typeof(DecimalEntry), 0);
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public bool ShouldReactToTextChanges { get; set; }
+
+        public DecimalEntry()
+        {
+            ShouldReactToTextChanges = true;
+        }
+
+        public static int DumbParse(string input)
+        {
+            if (input == null) return 0;
+
+            var number = 0;
+            int multiply = 1;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    number += (input[i] - '0') * (multiply);
+                    multiply *= 10;
+                }
+            }
+            return number;
+        }
+
+        public static string ConvertNumber(int value)
+        {
+            string number = String.Empty;
+
+            number = value.ToString().Replace(",", "").Replace(".", "");
+
+            if (number.Equals("")) number = "000";
+
+            number = number.PadLeft(3, '0');
+
+            if (number.Length > 3 && number.Substring(0, 1).Equals("0"))
+            {
+                number = number.Substring(1, number.Length - 1);
+            }
+
+            double finalValue = Convert.ToDouble(number) / 100;
+
+            return finalValue.ToString();
+        }
+
+    }
+}
