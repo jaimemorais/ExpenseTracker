5216f8e3dec97337933e3f24978a9dbfee7ea104
jaimemorais <jaimemorais@gmail.com>
2018-09-07 11:58:32 -0300

    Visual adjustments

--- ExpenseTrackerApp/ExpenseTrackerApp/Views/ExpenseCreatePage.xaml.cs
+++ ExpenseTrackerApp/ExpenseTrackerApp/Views/ExpenseCreatePage.xaml.cs
@@ -21,35 +21,20 @@ namespace ExpenseTrackerApp.Views
 
                 string carteira = "Carteira";
                 if (this.pckPaymentType.Items.Any(p => p == carteira))
-                    this.pckPaymentType.SelectedItem = carteira;
+                    this.pckPaymentType.SelectedItem = carteira;                                
+            }
+            else
+            {
+                EntryValue.Text = string.Empty;
+                EntryValue.Focus();
             }
         }
 
-        private void pckPaymentType_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
-            //EntryDescription.Focus();
-        }
 
         private void ContentPage_Appearing(object sender, System.EventArgs e)
         {
-            this.EntryValue.Text = string.Empty;            
+            EntryValue.Text = string.Empty;            
         }
         
-        private void EntryValue_Completed(object sender, System.EventArgs e)
-        {
-            // TODO remove
-            if (this.EntryValue.Text == "4.05")
-            {
-                string onibus = "Onibus";
-                if (this.pckCategory.Items.Any(c => c == onibus))
-                    this.pckCategory.SelectedItem = onibus;
-
-                string carteira = "Carteira";
-                if (this.pckPaymentType.Items.Any(p => p == carteira))
-                    this.pckPaymentType.SelectedItem = carteira;
-
-                this.EntryValue.Unfocus();
-            }
-        }
     }
 }
