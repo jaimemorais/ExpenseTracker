49e11217a3a95c8cafc71b676c4efd68c4b2ebe0
jaimemorais <jaimemorais@gmail.com>
2019-01-05 15:43:06 -0200

    Update appcenter build

--- ExpenseTrackerApp/ExpenseTrackerApp.Droid/appcenter-pre-build.sh
+++ ExpenseTrackerApp/ExpenseTrackerApp.Droid/appcenter-pre-build.sh
@@ -4,10 +4,10 @@ GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/ExpenseTrackerApp/ExpenseTrackerApp
 
 if [ -e "$GOOGLE_JSON_FILE" ]  
 then  
-    echo "Updating Google Json"
+    echo "Atualizando Google Json com a variavel de ambiente do appcenter"
     echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
     sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
 
-    echo "File content:"
+    echo "Conteudo para o arquivo google-services.json escrito:"
     cat $GOOGLE_JSON_FILE
 fi  
