1f3c4f1bc12234b0d35ce802be0be20511060654
Jaime Morais <jaimemorais@gmail.com>
2022-05-24 16:29:02 -0300

    Web editor test

--- ExpenseTrackerApp/ExpenseTrackerApp/Converters/ExpenseTrackerJsonDateConverter.cs
+++ ExpenseTrackerApp/ExpenseTrackerApp/Converters/ExpenseTrackerJsonDateConverter.cs
@@ -7,6 +7,7 @@ namespace ExpenseTrackerApp.Converters
         public ExpenseTrackerJsonDateConverter()
         {
             base.DateTimeFormat = "yyyy-MM-dd";
+            
         }
     }
 }
