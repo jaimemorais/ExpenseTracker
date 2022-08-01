ca70525dd264c1fa86a3379b2bfca5875c80e5e2
jaimemorais <jaimemorais@gmail.com>
2022-08-01 08:03:48 -0300

    Reorganize folder structure

---
+++ src/ExpenseTrackerWeb/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseTrackerWebApi.Helpers;
+using System.Configuration;
+using System.Net;
+using System.Web.Http;
+
+namespace ExpenseTrackerWebApi.Controllers
+{
+    public abstract class BaseApiController : ApiController
+    {
+        protected void CheckAuth()
+        {
+            // TODO auth
+            if (ApiUtils.GetHeaderValue(Request, "expensetracker-api-token") == null ||
+                ApiUtils.GetHeaderValue(Request, "expensetracker-api-token") != ConfigurationManager.AppSettings.Get("expensetracker-api-token"))
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
---
+++ src/ExpenseTrackerWeb/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseTrackerWebApi.Helpers;
+using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ExpenseTrackerWebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // API
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // MVC Web App
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            ModelBinders.Binders.Add(typeof(decimal?), new DecimalModelBinder());
+            ModelBinders.Binders.Add(typeof(DateTime), new DateTimeModelBinder());
+
+        }
+    }
+}
